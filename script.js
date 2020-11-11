c40a6e0690d6d9916fe35897a47f8f7104775d19
Caspento <Mrcaspento@gmail.com>
2020-11-11 17:30:38 -0500

    adding function that adds the current or past hours to css class color

--- script.js
+++ script.js
@@ -1,87 +1,78 @@
 window.onload = function () {
-  var memeTitle = document.getElementById("date");
-  var timeForMemes = document.getElementById("time");
+    var memeTitle = document.getElementById("date");
+    var timeForMemes = document.getElementById("time");
+    //var 
+    var time = moment().format("LL");// this the date format
+    //var not = $('.notes')
+    memeTitle.textContent = "" + time;
 
-  var time = moment().format("LL");// this the date format
-  //var not = $('.notes')
-  memeTitle.textContent = "" + time;
+    window.setInterval(function () {
+        timeForMemes.textContent = "Meme time: " + moment().format("LT");
+    }, 1000);
 
-  window.setInterval(function () {
-    timeForMemes.textContent = "Meme time: " + moment().format("LT");
-  }, 1000);
+    //To save notes in local storage
+    window.localStorage
+// make the current hour be green and all others red
+$(document).ready(function(){
 
-  $(".saveMemes").on("click", function (event) {
-    event.preventDefault();
-    var memeIndex = $(this).siblings(".notes")
-    memes[memeIndex].notes = $(this).notes
-    saveMemes();
-    displayMemes();
-  });
+})
+//define as future or as present
 
-  function saveMemes() {
-    localStorage.setItem("memes", JSON.stringify(memes));// save my meme notes to local storage
-  };
-  function displayMemes(){
-    memes.forEach(function(hour){
-      $('#memehour').val(hour.notes)
-    })
-  }
-  function init(){
-    var saveMemes = JSON.parse(localStorage.getItem('memes'));
-    if(saveMemes){
-      memes = saveMemes;
-    }
-    saveMemes();
-    displayMemes();
-  }
-  init();
-  // array for my memes
-  var memes = [
-    {
-      id: "9:00am",
-      notes: ""
-    },
-    {
-      id: "10:00am",
-      notes: ""
-    }, {
-      id: "11:00am",
-      notes: ""
-    }, {
-      id: "12:00pm",
-      notes: ""
-    }, {
-      id: "1:00pm",
-      notes: ""
-    }, {
-      id: "2:00pm",
-      notes: ""
-    }, {
-      id: "3:00pm",
-      notes: ""
-    }, {
-      id: "4:00pm",
-      notes: ""
-    }, {
-      id: "5:00pm",
-      notes: ""
-    }, {
-      id: "6:00pm",
-      notes: ""
-    }, {
-      id: "7:00pm",
-      notes: ""
-    }, {
-      id: "8:00pm",
-      notes: ""
-    }, {
-      id: "9:00pm",
-      notes: ""
-    }
-  ]
-  
-  
+    var noteToSave = [
+        {
+            id: "0",
+            hour: "09",
+            time: "09",
+            notes: ""
+        },
+        {
+            id: "1",
+            hour: "10",
+            time: "10",
+            notes: ""
+        },
+        {
+            id: "2",
+            hour: "11",
+            time: "11",
+            notes: ""
+        },
+        {
+            id: "3",
+            hour: "12",
+            time: "12",
+            notes: ""
+        },
+        {
+            id: "4",
+            hour: "01",
+            time: "13",
+            notes: ""
+        },
+        {
+            id: "5",
+            hour: "02",
+            time: "14",
+            notes: ""
+        },
+        {
+            id: "6",
+            hour: "03",
+            time: "15",
+            notes: ""
+        },
+        {
+            id: "7",
+            hour: "04",
+            time: "16",
+            notes: ""
+        },
+        {
+            id: "8",
+            hour: "05",
+            time: "17",
+            notes: ""
+        },
 
- 
-
-};
+    ]
+}
