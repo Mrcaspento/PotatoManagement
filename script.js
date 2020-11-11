9f9907b1ebf7a8f0c76972adf069e2ad86ab8cd6
Caspento <Mrcaspento@gmail.com>
2020-11-11 16:49:20 -0500

    new store function

--- script.js
+++ script.js
@@ -1,35 +1,87 @@
-window.onload = function()
-{
-  var title = document.getElementById("date");
-  var timenow = document.getElementById("time");
-    
-  var time = moment().format("DD-MM-YYYY");
-  
-  title.textContent = "" + time;
-  
-  window.setInterval(function() {
-    timenow.textContent = "Meme time: " + moment().format("h:mm:ss");
+window.onload = function () {
+  var memeTitle = document.getElementById("date");
+  var timeForMemes = document.getElementById("time");
+
+  var time = moment().format("LL");// this the date format
+  //var not = $('.notes')
+  memeTitle.textContent = "" + time;
+
+  window.setInterval(function () {
+    timeForMemes.textContent = "Meme time: " + moment().format("LT");
   }, 1000);
-  
-};
 
-function saveNotes(){
-    localStorage.setItem("memes",JSON.stringify(memes));// save my meme notes to local storage
-};
-
-var memes = [
-    {id:"9:00am",
-    
-}
-]
-
-intit();
-
-$(".saveBtn").on("click", function(event) {
+  $(".saveMemes").on("click", function (event) {
     event.preventDefault();
-    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-    memes[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
-    console.log(saveIndex);
-    saveReminders();
-    displayReminders();
-})
+    var memeIndex = $(this).siblings(".notes")
+    memes[memeIndex].notes = $(this).notes
+    saveMemes();
+    displayMemes();
+  });
+
+  function saveMemes() {
+    localStorage.setItem("memes", JSON.stringify(memes));// save my meme notes to local storage
+  };
+  function displayMemes(){
+    memes.forEach(function(hour){
+      $('#memehour').val(hour.notes)
+    })
+  }
+  function init(){
+    var saveMemes = JSON.parse(localStorage.getItem('memes'));
+    if(saveMemes){
+      memes = saveMemes;
+    }
+    saveMemes();
+    displayMemes();
+  }
+  init();
+  // array for my memes
+  var memes = [
+    {
+      id: "9:00am",
+      notes: ""
+    },
+    {
+      id: "10:00am",
+      notes: ""
+    }, {
+      id: "11:00am",
+      notes: ""
+    }, {
+      id: "12:00pm",
+      notes: ""
+    }, {
+      id: "1:00pm",
+      notes: ""
+    }, {
+      id: "2:00pm",
+      notes: ""
+    }, {
+      id: "3:00pm",
+      notes: ""
+    }, {
+      id: "4:00pm",
+      notes: ""
+    }, {
+      id: "5:00pm",
+      notes: ""
+    }, {
+      id: "6:00pm",
+      notes: ""
+    }, {
+      id: "7:00pm",
+      notes: ""
+    }, {
+      id: "8:00pm",
+      notes: ""
+    }, {
+      id: "9:00pm",
+      notes: ""
+    }
+  ]
+  
+  
+
+ 
+
+};
